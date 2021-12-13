6afd47f39016cbb54cb2b88be46f953f50760192
Nathan PODESTA <nathan.podesta@icloud.com>
2021-12-13 01:08:55 +0100

    user profil

--- src/app/pages/image-infos/image-infos.component.ts
+++ src/app/pages/image-infos/image-infos.component.ts
@@ -29,26 +29,18 @@ export class ImageInfosComponent implements OnInit {
       if (data.stat == 'ok') {
         this.showProgressBar = false;
         this.image = data;
-        console.log(data);
-        
+
         this.flickrSearch.getInformationsUser(this.image.photo.owner.nsid).subscribe((data) => {
-          console.log("USER");
-          console.log(data);
           this.user = data;
         });
 
         this.flickrSearch.getPhotosUser(this.image.photo.owner.nsid).subscribe((data) => {
-          console.log("photosUSER");
-          console.log(data);
           this.userPhotos = data;
         });
-
       }
     });
     this.flickrSearch.getComments(this.imageId).subscribe((data) => {
       this.comments = data;
-      console.log(this.comments);
-      console.log(data);
     });
   }
 }
