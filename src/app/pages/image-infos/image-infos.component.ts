9225320e829bbc2b1cf78847e4e122835b7bb536
adrien morvan <adrien.morvan@laposte.fr>
2021-12-17 09:08:30 +0100

    Add options

--- src/app/pages/image-infos/image-infos.component.ts
+++ src/app/pages/image-infos/image-infos.component.ts
@@ -73,6 +73,44 @@ export class ImageInfosComponent implements OnInit {
           center: [+this.location.longitude, +this.location.latitude], // starting position [lng, lat]
           zoom: 9, // starting zoom
         });
+        this.map.addControl(new mapboxgl.NavigationControl( {
+          showZoom : true,
+          showCompass: true,
+          visualizePitch: true
+        }),);
+        this.map.addLayer({
+        id: "3d-buildings",
+        source: "composite",
+        "source-layer": "building",
+        filter: ["==", "extrude", "true"],
+        type: "fill-extrusion",
+        minzoom: 15,
+        paint: {
+          "fill-extrusion-color": "#aaa",
+
+          // use an 'interpolate' expression to add a smooth transition effect to the
+          // buildings as the user zooms in
+          "fill-extrusion-height": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            15,
+            0,
+            15.05,
+            ["get", "height"],
+          ],
+          "fill-extrusion-base": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            15,
+            0,
+            15.05,
+            ["get", "min_height"],
+          ],
+          "fill-extrusion-opacity": 0.6,
+        },
+      });
   
         // const popup = new mapboxgl.Popup({ offset: 25 }) // add popups
         //   .setHTML(``);
